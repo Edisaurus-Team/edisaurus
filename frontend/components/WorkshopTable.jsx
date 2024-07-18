19d65d6ab0e8db675bbf04d2722cb4f83e5b532a
ThomasMoncrief <jtmoncri@gmail.com>
2024-07-18 02:48:35 -0500

    trash icon confirm button

--- frontend/components/WorkshopTable.jsx
+++ frontend/components/WorkshopTable.jsx
@@ -1,38 +1,45 @@
 import React, { useState, useEffect } from 'react';
-import trashIcon from "../images/trash_icon.png";
+// import trashIcon from "../images/trash_icon.png";
 import { FaRegTrashCan } from "react-icons/fa6";
+import { FaCheck } from "react-icons/fa6";
+import "../css/workshopTableStyle.css"
 
 export default function Table () {
 
     const[content, setContent] = useState([]);
+    const[clickedIcon, setClickedIcon] = useState([]);
 
     async function updateTable() {
-        try {
-            const response = await fetch('/api/get_table/');
-            const data = await response.json();
-            setContent(data)
-        } catch (error) {
-            console.error('Error:', error);
-        }
+      try {
+        const response = await fetch('/api/get_table/');
+        const data = await response.json();
+        setContent(data)
+      } catch (error) {
+        console.error('Error:', error);
+      }
     }
-
     useEffect(() => {
-        updateTable();
+      updateTable();
     }, []);
  
-
+    function handleTrashIconClick(id) {
+      setClickedIcon({
+        id: (clickedIcon.id === id ? null : id)
+      })
+    };
     
-    function trashIconEvent(id) {
+    function deleteArticle(id) {
+        console.log(`article ${id} deleted`)
         fetch('/api/workshop_api/' + id, {
-            method: 'DELETE',
+          method: 'DELETE',
         })
         .then(response => {
-            if (response.ok) {
-              const updatedContent = content.filter(article => article.id !== id);
-              setContent(updatedContent);
-            } else {
-              console.error('Error: article not found.');
-            }
+          if (response.ok) {
+            const updatedContent = content.filter(article => article.id !== id);
+            setContent(updatedContent);
+          } else {
+            console.error('Error: article not found.');
+          }
         })
     }
 
@@ -42,8 +49,8 @@ export default function Table () {
       <table>
       <thead>
       <tr>
-          <th>ID</th>
-          <th>Title</th>
+        <th>ID</th>
+        <th>Title</th>
       </tr>
       </thead>
       <tbody>
@@ -52,9 +59,21 @@ export default function Table () {
             <td>{article.id}</td>
             <td><a href={`/workshop/${article.id}`}>{article.title}</a></td>
             <td>
-              <span onClick={() => trashIconEvent(article.id)} id={article.id} className="trash-wrapper">
-                
-                <FaRegTrashCan className="icon" />
+              <span id={article.id} 
+                className={article.id === clickedIcon.id ? "trash-wrapper selected" : "trash-wrapper"} 
+                onClick={() => handleTrashIconClick(article.id)}
+              >
+                <FaRegTrashCan className="icon trash-icon" />
+              </span>
+            </td>
+            <td>
+              <span id={article.id} 
+                style={{visibility: article.id == clickedIcon.id ? 'visible' : 'hidden'}}
+                onClick={() => deleteArticle(article.id)}
+              >
+                <FaCheck  
+                  className="icon"
+                />
               </span>
             </td>
           </tr>
