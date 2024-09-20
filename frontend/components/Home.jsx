38f8f703b7625b2060ac9c0e2e4d9f8f75e868ce
ThomasMoncrief <jtmoncri@gmail.com>
2024-09-20 00:12:40 -0500

    home page edit

--- frontend/components/Home.jsx
+++ frontend/components/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
         
     </div>
     <h2 className="about-header"><a href="/uploader/">Uploader</a></h2>
-    <p>Get started by uploading a document.</p>
+    <p>Get started by pasting in some text.</p>
     <h2 className="about-header"><a href="/workshop/">Workshop</a></h2>
     <p>Review changes in the workshop.</p>
     <h2 className="about-header"><a href="/about/">About</a></h2>
